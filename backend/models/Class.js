36e434e2f66faed27c49d94a8901b3f3ab818116
Gopalcode <67701373+Gopalcode@users.noreply.github.com>
2026-08-21 15:15:54 +0530

    Class.js update
    Class.js update

--- backend/models/Class.js
+++ backend/models/Class.js
@@ -1,23 +1,41 @@
 const mongoose = require("mongoose");
 
 const classSchema = new mongoose.Schema({
+
   className: String,
+
   imgUrl: String,
+
   imgUrl2: String,
+
   educator: String,
+
   exam: String,
+
   subject: String,
+
   appName: String,
+
   books: String,
+
   offer: String,
+
   appLink: String,
+
+  demoVideo: String,
+
   yt: String,
-  demoVideo:String,
+
   tg: String,
+
   wa: String,
+
   ig: String,
-  io:String,
-  wb:String
+
+  io: String,
+
+  wb: String
+
 });
 
 module.exports = mongoose.model("Class", classSchema);
