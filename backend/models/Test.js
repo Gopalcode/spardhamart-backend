c90c70a7630581bf551dc29202ac5bbeba8ef72e
Gopalcode <67701373+Gopalcode@users.noreply.github.com>
2026-09-08 16:51:58 +0530

    update on test.js
    update on test.js

--- backend/models/Test.js
+++ backend/models/Test.js
@@ -1,18 +1,39 @@
 const mongoose = require("mongoose");
 
 const testSchema = new mongoose.Schema({
+
   className: String,
+
   exam: String,
+
   subject: String,
+
   appName: String,
+
   offer: String,
+
   appLink: String,
+
   imgUrl: String,
+
   yt: String,
+
   tg: String,
+
   wa: String,
+
   io: String,
-  wb: String
+
+  wb: String,
+
+  // =========================================
+  // DRAFT / PUBLISH
+  // =========================================
+  status: {
+    type: Boolean,
+    default: true
+  }
+
 });
 
 module.exports = mongoose.model("Test", testSchema);
